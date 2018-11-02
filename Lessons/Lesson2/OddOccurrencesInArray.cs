e7ed3761cccfbfc48d3b3857b08279bfcea3d5cb
Yamakawa, Atsushi <atsushi.yamakawa@clarivate.com>
2018-11-02 10:55:34 +0900

    Lesson2 completed.

--- Lessons/Lesson2/OddOccurrencesInArray.cs
+++ Lessons/Lesson2/OddOccurrencesInArray.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace Lessons.Lesson2
 {
-    class OddOccurrencesInArray
+    public class OddOccurrencesInArray
     {
+        public int Solution(int[] A)
+        {
+            var occurrences = new HashSet<int>();
 
+            foreach(var num in A)
+            {
+                if (occurrences.Contains(num))
+                {
+                    occurrences.Remove(num);
+                }
+                else
+                {
+                    occurrences.Add(num);
+                }
+            }
+
+            return occurrences.Take(1).First();
+        }
     }
 }
---
+++ LessonsTest/Lesson2/OddOccurrencesInArrayTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson2
+{
+    [TestClass]
+    public class OddOccurrencesInArrayTest
+    {
+        [TestMethod]
+        public void TestSample()
+        {
+            var testClass = new OddOccurrencesInArray();
+
+            Assert.AreEqual(7, testClass.Solution(new int[] { 9, 3, 9, 3, 9, 7, 9 }));
+        }
+
+        [TestMethod]
+        public void TestNequalsOne()
+        {
+            var testClass = new OddOccurrencesInArray();
+
+            Assert.AreEqual(9, testClass.Solution(new int[] { 9 }));
+        }
+
+        [TestMethod]
+        public void TestNequalsMax()
+        {
+            var testClass = new OddOccurrencesInArray();
+
+            int[] args = new int[999999];
+            for(int i=0; i<args.Length; i++)
+            {
+                args[i] = 1;
+            }
+
+            Assert.AreEqual(1, testClass.Solution(args));
+        }
+
+        [TestMethod]
+        public void TestElementlsMax()
+        {
+            var testClass = new OddOccurrencesInArray();
+
+            Assert.AreEqual(1000000000, testClass.Solution(new int[] { 1000000000, 1, 1 }));
+        }
+    }
+}
