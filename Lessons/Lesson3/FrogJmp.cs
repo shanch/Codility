4572756818c9df0727e84149506f283b93658f14
shanch <shanch@sweet.ocn.ne.jp>
2018-11-09 11:26:03 +0900

    Task2 completed.

---
+++ Lessons/Lesson3/FrogJmp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson3
+{
+    public class FrogJmp
+    {
+        public int Solution(int X, int Y, int D)
+        {
+            return (Y - X + D - 1) / D;
+        }
+
+        /*public int Solution(int X, int Y, int D)
+        {
+            return (int)Math.Ceiling((double)(Y - X) / (double)D);
+        }*/
+
+        /*public int Solution(int X, int Y, int D)
+        {
+            int count = 1;
+
+            while(X + D * count < Y)
+            {
+                count++;
+            }
+
+            return count;
+        }*/
+    }
+}
---
+++ LessonsTest/Lesson3/FrogJmpTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson3
+{
+    [TestClass]
+    public class FrogJmpTest
+    {
+        [TestMethod]
+        public void TestSample()
+        {
+            var testClass = new FrogJmp();
+
+            Assert.AreEqual(3, testClass.Solution(10, 85, 30));
+        }
+
+        [TestMethod]
+        public void TestXequalsY()
+        {
+            var testClass = new FrogJmp();
+
+            Assert.AreEqual(3, testClass.Solution(10, 100, 30));
+        }
+
+        [TestMethod]
+        public void TestMaxBoundary()
+        {
+            var testClass = new FrogJmp();
+
+            Assert.AreEqual(1, testClass.Solution(1000000000 - 1, 1000000000, 1000000000));
+        }
+
+        [TestMethod]
+        public void TestPerformance()
+        {
+            var testClass = new FrogJmp();
+
+            Assert.AreEqual(999999999, testClass.Solution(1, 1000000000, 1));
+        }
+    }
+}
