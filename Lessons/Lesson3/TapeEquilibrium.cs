b0bba40943361de2b98321b26f08a5b5ae91269e
shanch <shanch@sweet.ocn.ne.jp>
2018-11-14 13:39:31 +0900

    Complete lesson3

---
+++ Lessons/Lesson3/TapeEquilibrium.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson3
+{
+    public class TapeEquilibrium
+    {
+        public int Solution(int[] A)
+        {
+            int sumOfFirstPart = A[0];
+            int sumOfSecondPart = A.Skip(1).Sum();
+
+            int min = Math.Abs(sumOfFirstPart - sumOfSecondPart);
+
+            for (int p = 1; p < A.Length - 1; p++)
+            {
+                sumOfFirstPart += A[p];
+                sumOfSecondPart -= A[p];
+
+                int diff = Math.Abs(sumOfFirstPart - sumOfSecondPart);
+
+                if (diff < min)
+                {
+                    min = diff;
+                }
+            }
+
+            return min;
+        }
+
+        public int SolutionVer1(int[] A)
+        {
+            int min = int.MaxValue;
+
+            for (int p = 1; p < A.Length - 1; p++)
+            {
+                int sumOfFirstPart = A.Take(p).Sum();
+                int sumOfSecondPart = A.Skip(p).Sum();
+
+                int diff = Math.Abs(sumOfFirstPart - sumOfSecondPart);
+
+                if (diff < min)
+                {
+                    min = diff;
+                }
+            }
+
+            return min;
+        }
+
+        public int SolutionVer2(int[] A)
+        {
+            int sumOfFirstPart = A[0];
+            int sumOfSecondPart = A.Skip(1).Sum();
+
+            int min = int.MaxValue;
+
+            int diff = Math.Abs(sumOfFirstPart - sumOfSecondPart);
+
+            for (int p = 1; p < A.Length - 1; p++)
+            {
+                diff = Math.Abs(diff - A[p] * 2);
+
+                if (diff < min)
+                {
+                    min = diff;
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ LessonsTest/Lesson3/TapeEquilibriumTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson3
+{
+    [TestClass]
+    public class TapeEquilibriumTest
+    {
+        [TestMethod]
+        public void TestSsample()
+        {
+            var testClass = new TapeEquilibrium();
+
+            Assert.AreEqual(1, testClass.Solution(new int[] { 3, 1, 2, 4, 3 }));
+        }
+
+        [TestMethod]
+        public void TestPerformance()
+        {
+            var testClass = new TapeEquilibrium();
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                testClass.Solution(new int[] { 3, 1, 2, 4, 3 });
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed.Milliseconds);
+
+            sw.Restart();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                testClass.SolutionVer1(new int[] { 3, 1, 2, 4, 3 });
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed.Milliseconds);
+
+            sw.Restart();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                testClass.SolutionVer2(new int[] { 3, 1, 2, 4, 3 });
+            }
+
+            sw.Stop();
+
+            Console.WriteLine(sw.Elapsed.Milliseconds);
+        }
+
+        [TestMethod]
+        public void TestMinN()
+        {
+            var testClass = new TapeEquilibrium();
+
+            Assert.AreEqual(2, testClass.Solution(new int[] { 3, 1 }));
+        }
+
+        [TestMethod]
+        public void TestMinNPlus1()
+        {
+            var testClass = new TapeEquilibrium();
+
+            Assert.AreEqual(0, testClass.Solution(new int[] { 3, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void TestMaxN()
+        {
+            var testClass = new TapeEquilibrium();
+
+            Assert.AreEqual(0, testClass.Solution(Enumerable.Repeat(1, 100000).ToArray()));
+        }
+
+        [TestMethod]
+        public void TestMaxElement()
+        {
+            var testClass = new TapeEquilibrium();
+
+            Assert.AreEqual(0, testClass.Solution(Enumerable.Repeat(1000, 100000).ToArray()));
+        }
+
+        [TestMethod]
+        public void TestMinElement()
+        {
+            var testClass = new TapeEquilibrium();
+
+            Assert.AreEqual(0, testClass.Solution(Enumerable.Repeat(-1000, 100000).ToArray()));
+        }
+    }
+}
