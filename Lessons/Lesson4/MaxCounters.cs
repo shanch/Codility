40fd75f5a13c16ef86f3378fac18caefb02e38b7
shanch <shanch@sweet.ocn.ne.jp>
2018-11-19 16:17:12 +0900

    Task 1 and Task 3 of Lesson 4 are completed.

---
+++ Lessons/Lesson4/MaxCounters.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson4
+{
+    public class MaxCounters
+    {
+        public int[] Solution(int N, int[] A)
+        {
+            var counters = new int[N];
+
+            var max = 0;
+            var baseline = 0;
+
+            foreach(int a in A)
+            {
+                if(a >= 1 && a <= N)
+                {
+                    if(counters[a - 1] < baseline)
+                    {
+                        counters[a - 1] = baseline + 1;
+                    }
+                    else
+                    {
+                        counters[a - 1]++;
+                    }
+
+                    if (counters[a - 1] > max)
+                    {
+                        max = counters[a - 1];
+                    }
+                }
+                else if(a == N + 1)
+                {
+                    baseline = max;
+                }
+            }
+
+            for(int i=0; i<N; i++)
+            {
+                if(counters[i] < baseline)
+                {
+                    counters[i] = baseline;
+                }
+            }
+
+            return counters;
+        }
+
+        public int[] SolutionSlow(int N, int[] A)
+        {
+            var counters = new int[N];
+
+            var max = 0;
+
+            foreach (int a in A)
+            {
+                if (a >= 1 && a <= N)
+                {
+                    // increase
+                    counters[a - 1]++;
+
+                    if (counters[a - 1] > max)
+                    {
+                        max = counters[a - 1];
+                    }
+                }
+                else if (a == N + 1)
+                {
+                    // max counter
+                    for (int i = 0; i < N; i++)
+                    {
+                        counters[i] = max;
+                    }
+                }
+            }
+
+            return counters;
+        }
+    }
+}
---
+++ LessonsTest/Lesson4/PermCheckTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Lessons.Lesson4
+{
+    [TestClass]
+    public class PermCheckTest
+    {
+        [TestMethod]
+        public void TestSamplePerm()
+        {
+            var testClass = new PermCheck();
+
+            Assert.AreEqual(1, testClass.Solution(new int[] { 4, 1, 3, 2 }));
+        }
+
+        [TestMethod]
+        public void TestSampleNonPerm()
+        {
+            var testClass = new PermCheck();
+
+            Assert.AreEqual(0, testClass.Solution(new int[] { 4, 1, 3 }));
+        }
+
+        [TestMethod]
+        public void TestMinArrayLengthPerm()
+        {
+            var testClass = new PermCheck();
+
+            Assert.AreEqual(1, testClass.Solution(new int[] { 1 }));
+        }
+
+        [TestMethod]
+        public void TestMinArrayLengthNonPerm()
+        {
+            var testClass = new PermCheck();
+
+            Assert.AreEqual(0, testClass.Solution(new int[] { 4 }));
+        }
+
+        [TestMethod]
+        public void TestMaxArrayLengthPerm()
+        {
+            var testClass = new PermCheck();
+
+            Assert.AreEqual(1, testClass.Solution(Enumerable.Range(1, 100000).ToArray()));
+        }
+
+        [TestMethod]
+        public void TestMaxArrayLengthNonPerm()
+        {
+            var testClass = new PermCheck();
+
+            Assert.AreEqual(0, testClass.Solution(Enumerable.Concat(Enumerable.Range(1, 99999), new int[] { 1 }).ToArray()));
+        }
+    }
+}
