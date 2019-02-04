9e41f2bf1bd928e7b6e8da545e1b48dd73075d43
shanch <shanch@sweet.ocn.ne.jp>
2019-02-04 16:06:30 +0900

    The last task of lesson 4 is done.

---
+++ Lessons/Lesson4/MissingInteger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson4
+{
+    public class MissingInteger
+    {
+        public int Solution(int[] A)
+        {
+            bool[] b = new bool[A.Length];
+
+            for(int i=0; i<A.Length; i++)
+            {
+                if(A[i] < 1 || A.Length < A[i])
+                {
+                    continue;
+                }
+
+                b[A[i] - 1] = true;
+            }
+
+            for (int i=0; i<b.Length; i++)
+            {
+                if (!b[i]) return i + 1;
+            }
+
+            return A.Length + 1;
+        }
+    }
+}
---
+++ LessonsTest/Lesson4/MissingIntegerTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson4
+{
+    [TestClass]
+    public class MissingIntegerTest
+    {
+        [TestMethod]
+        public void TestSample1()
+        {
+            var testClass = new MissingInteger();
+
+            var actual = testClass.Solution(new int[] { 1, 3, 6, 4, 1, 2 });
+
+            Assert.AreEqual(5, actual);
+        }
+
+        [TestMethod]
+        public void TestSample2()
+        {
+            var testClass = new MissingInteger();
+
+            var actual = testClass.Solution(new int[] { 1, 2, 3 });
+
+            Assert.AreEqual(4, actual);
+        }
+
+        [TestMethod]
+        public void TestSample3()
+        {
+            var testClass = new MissingInteger();
+
+            var actual = testClass.Solution(new int[] { -1, -3 });
+
+            Assert.AreEqual(1, actual);
+        }
+
+    }
+}
