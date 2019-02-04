f9dc1102b968a2f161875fe50a5f170fd618e506
shanch <shanch@sweet.ocn.ne.jp>
2019-02-04 12:28:53 +0900

    Task 2 of lesson 4 is done.

---
+++ Lessons/Lesson4/FrogRiverOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson4
+{
+    public class FrogRiverOne
+    {
+        public int Solution(int X, int[] A)
+        {
+            HashSet<int> hs = new HashSet<int>(Enumerable.Range(1, X));
+
+            for(int i=0; i<A.Length; i++)
+            {
+                hs.Remove(A[i]);
+
+                if (hs.Count == 0) return i; 
+            }
+
+            return -1;
+
+        }
+    }
+}
