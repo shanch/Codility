15179dab8ed27bd565384b1177f44e4a7c6af2dc
shanch <shanch@sweet.ocn.ne.jp>
2019-02-10 21:49:37 +0900

    in the process of task1

---
+++ Lessons/Lesson8/EquiLeader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson8
+{
+    class EquiLeader
+    {
+        public int Solution(int[] A)
+        {
+            int cnt = 0;
+
+            for(int i=0; i<A.Length; i++)
+            {
+                int leader1 = GetLeader(A.Take(i + 1).ToArray());
+                int leader2 = GetLeader(A.Skip(i + 1).ToArray());
+
+                if (leader1 == leader2) cnt++;
+            }
+
+            return cnt;
+        }
+
+        private int GetLeader(int[] aa)
+        {
+            aa = aa.OrderBy(a => a).ToArray();
+
+            int lenOfLeader = aa.Length / 2 + 1;
+
+            for(int i=0; i<aa.Length - lenOfLeader + 1; i++)
+            {
+                if (aa[i] == aa[i + lenOfLeader - 1]) return aa[i];
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LessonsTest/Lesson8/EquiLeaderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson8
+{
+    [TestClass]
+    public class EquiLeaderTest
+    {
+        [TestMethod]
+        public void TestSample()
+        {
+            var test = new EquiLeader();
+
+            Assert.AreEqual(2, test.Solution(new int[] { 4, 3, 4, 4, 4, 2 }));
+
+        }
+
+        [TestMethod]
+        public void TestSample2()
+        {
+            var test = new EquiLeader();
+
+            Assert.AreEqual(4, test.Solution(new int[] { 4, 3, 4, 4, 4, 2, 4 }));
+
+        }
+    }
+}
