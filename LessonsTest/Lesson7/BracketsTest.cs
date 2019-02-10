6859f49ed2fe8897372a3679e8e1734ab03cf622
shanch <shanch@sweet.ocn.ne.jp>
2019-02-10 14:58:45 +0900

    Task1 id done.

---
+++ LessonsTest/Lesson7/BracketsTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson7
+{
+    [TestClass]
+    public class BracketsTest
+    {
+        [TestMethod]
+        public void TestSample()
+        {
+            var test = new Brackets();
+
+            Assert.AreEqual(1, test.Solution("{[()()]}"));
+        }
+
+        [TestMethod]
+        public void TestSample2()
+        {
+            var test = new Brackets();
+
+            Assert.AreEqual(0, test.Solution("([)()]"));
+        }
+
+        [TestMethod]
+        public void TestSample3()
+        {
+            var test = new Brackets();
+
+            Assert.AreEqual(0, test.Solution(")("));
+        }
+
+        [TestMethod]
+        public void TestSample4()
+        {
+            var test = new Brackets();
+
+            Assert.AreEqual(0, test.Solution("{{{{"));
+        }
+    }
+}
