56d2eb79b350786842958c6f328084f9ff805c9f
shanch <shanch@sweet.ocn.ne.jp>
2019-02-11 15:31:45 +0900

    Task1 is done.

---
+++ Lessons/Lesson9/MaxSliceSum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Lesson9
+{
+    class MaxSliceSum
+    {
+        public int Solution(int[] A)
+        {
+            int maxHere = A[0];
+            int maxSofar = A[0];
+
+            for(int i=1; i<A.Length; i++)
+            {
+                maxHere = Math.Max(A[i], maxHere + A[i]);
+                maxSofar = Math.Max(maxSofar, maxHere);
+            }
+
+            return maxSofar;
+        }
+
+        public int SlowSolution(int[] A)
+        {
+            int max = int.MinValue;
+
+            for (int i = 0; i < A.Length - 1; i++)
+            {
+                int sum = A[i];
+
+                if (sum > max) max = sum;
+
+                for (int j = i + 1; j < A.Length; j++)
+                {
+                    sum += A[j];
+
+                    if (sum > max) max = sum;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LessonsTest/Lesson9/MaxSliceSumTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lessons.Lesson9
+{
+    [TestClass]
+    public class MaxSliceSumTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testClass = new MaxSliceSum();
+
+            Assert.AreEqual(5, testClass.Solution(new int[] { 3, 2, -6, 4, 0 }));
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var testClass = new MaxSliceSum();
+
+            Assert.AreEqual(int.MinValue, testClass.Solution(new int[] { int.MinValue }));
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var testClass = new MaxSliceSum();
+
+            Assert.AreEqual(1000000, testClass.Solution(Enumerable.Repeat(1,1000000).ToArray()));
+
+        }
+    }
+}
